24f27b260f347b4deb0a28fda93f9c1c093a00b3
Daniel Rothmann <danilarothmann@gmail.com>
2024-12-20 13:39:28 +0300

    Добавлена проверка на ввод числа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
 
     public static void carDataInput() {
         System.out.println("Введите количество машин участвующих в гонке: ");
-        int countOfCars = scanner.nextInt();
+        int countOfCars = checkNumberAndNonNegative();
         for (int i = 1; i <= countOfCars; i++) {
             System.out.println("Введите название машины № " + i + ":");
             String name = scanner.next();
@@ -23,16 +24,34 @@ public class Main {
         }
     }
 
-    public static int inputSpeedAndCheck(int numberOfCar) { // принимает номер машины
+    public static int inputSpeedAndCheck(int numberOfCar) {
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите скорость машины №:" + numberOfCar + ":");
-            int speed = scanner.nextInt();
-            if (speed < 0 || speed > 250) {
-                System.out.println("Неправильное значение. Введите корректное значение скорости: ");
+            System.out.println("Введите скорость машины № " + numberOfCar + ":");
+            int speed = checkNumberAndNonNegative();
+            if (speed > 250) {
+                System.out.println("Неправильное значение. Введите корректное значение скорости: 0-250 ");
             } else {
                 return speed;
             }
         }
+    }
+
+    public static int checkNumberAndNonNegative() {
+        boolean validInput = false;
+        int console = 0;
+        while (!validInput) {
+            try {
+                console = scanner.nextInt();
+                if (console < 0) {
+                    System.out.println("Ошибка: введите положительное целое число.");
+                } else {
+                    validInput = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка: введите корректное целое число.");
+                scanner.next();
+            }
+        }
+        return console;
     }
 }
